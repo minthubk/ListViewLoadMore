97383855da8c3beb6073071d22ed56411825302f
沈为伟 <weiweishen@anjuke.com>
2014-12-09 21:35:30 +0800

    增加刷新功能

---
+++ app/src/main/java/fyales/listviewloadmore/fragment/IndexFragment.java
@@ -0,0 +1,80 @@
+package fyales.listviewloadmore.fragment;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fyales.listviewloadmore.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IndexFragment extends Fragment {
+
+
+    public IndexFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onInflate(Activity activity, AttributeSet attrs, Bundle savedInstanceState) {
+        super.onInflate(activity, attrs, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_index, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
